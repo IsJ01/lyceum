572c6990fd1d4750ebda66eba09394503451f5cc
Администратор <islamishtalbagamaev27@gmail.com>
2024-02-13 17:24:18 +0300

    Thirteen Commit

--- lyceum/catalog/apps.py
+++ lyceum/catalog/apps.py
@@ -1,6 +1,9 @@
+"""Doc."""
 from django.apps import AppConfig
 
 
 class CatalogConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "catalog"
+    """Doc."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'catalog'
--- lyceum/homepage/apps.py
+++ lyceum/homepage/apps.py
@@ -1,6 +1,9 @@
+"""Doc."""
 from django.apps import AppConfig
 
 
 class HomepageConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "homepage"
+    """Doc."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'homepage'
